1881244c037dcd0c277bef3d0ad4a656c01a5b90
Damien Guard <damieng@gmail.com>
2019-10-08 10:22:51 -0700

    Update PR-Process for direct update, latest octokit etc.

--- pr-process/app.js
+++ pr-process/app.js
@@ -2,15 +2,15 @@ const Octokit = require('@octokit/rest');
 const cp = require('child_process');
 
 // Essential settings - change these as we can't have defaults
-const personalAccessToken = 'your personal access token';
-const reviewTeam = '@org/team';
+const personalAccessToken = '';
+const reviewTeam = '';
 const committer = {
-    name: 'Your Name',
-    email: 'Your Email'
+    name: '',
+    email: ''
 };
 
 // Optional tweaks - these are sensible defaults
-const branch = 'add-codeowners';
+const prBranch = 'add-codeowners';
 const gitHubUrl = 'github.com'; // Change this if GitHub Enterprise
 const message = 'Setup the CODEOWNERS for pull request reviews';
 const labelsToCreate = [
@@ -21,9 +21,11 @@ const labelsToCreate = [
 ];
 
 // Regular source, should not need to change
-var startCommand = process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open';
-var openUrl = (url) => cp.exec(`${startCommand} ${url}`);
-var octokit = new Octokit(
+const content = Buffer.from(`*\t${reviewTeam}\n`).toString('base64');
+const codeOwnersPath = '.github/CODEOWNERS';
+const startCommand = process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open';
+const openUrl = (url) => cp.exec(`${startCommand} ${url}`);
+const octokit = new Octokit(
     {
         auth: personalAccessToken,
         userAgent: 'PR Process Script',
@@ -39,9 +41,17 @@ var octokit = new Octokit(
 );
 
 async function run(owner, repo) {
-    const path = '.github/CODEOWNERS';
-    const content = Buffer.from(`*\t${reviewTeam}\n`).toString('base64');    
+    if (!personalAccessToken || !reviewTeam || !committer.name || !committer.email)
+        throw Error('Must setup essential settings before running script.')
 
+    await createLabels(owner, repo);
+    // Use this to setup a new flow with a PR
+    await createCodeOwnersPR(owner, repo);
+    // OR this one to modify and existing directly
+//    await updateCodeOwnersDirect(owner, repo);
+}
+
+async function createLabels(owner, repo) {
     console.log('Creating labels ...');
     try {
         await Promise.all(labelsToCreate
@@ -52,23 +62,34 @@ async function run(owner, repo) {
     {
         console.log('One or more labels probably existed (Go check)');
     }
+}
 
-    console.log('Creating Pull Request to add .github/CODEOWNERS ...');
+async function updateCodeOwnersDirect(owner, repo) {
+    const repository = (await octokit.repos.get({ owner, repo })).data;
+    const defaultBranch = repository.default_branch;
+    console.log(`Updating ${codeOwnersPath} on ${defaultBranch} ...`);
+    const existing = (await octokit.repos.getContents({owner, repo, path: codeOwnersPath, branch: defaultBranch})).data;
+    const updateFile = await octokit.repos.createOrUpdateFile(
+        { owner, repo, path: codeOwnersPath, message, content, committer, author: committer, branch: defaultBranch, sha: existing.sha })
+}
+
+async function createCodeOwnersPR(owner, repo) {
+    console.log(`Creating Pull Request to add ${codeOwnersPath} ...`);
     const repository = (await octokit.repos.get({ owner, repo })).data;
     const latest = (await octokit.repos.listCommits({ owner, repo, sha: repository.default_branch, per_page: 1 })).data[0];
-    const newBranch = (await octokit.git.createRef({ owner, repo, ref: 'refs/heads/' + branch, sha: latest.sha})).data;
+    const newBranch = (await octokit.git.createRef({ owner, repo, ref: 'refs/heads/' + prBranch, sha: latest.sha})).data;
     try {
-      const newFile = (await octokit.repos.createFile(
-          { owner, repo, path, message, content, committer, author: committer, branch }
+      const createFile = (await octokit.repos.createOrUpdateFile(
+          { owner, repo, path: codeOwnersPath, message, content, committer, author: committer, branch: prBranch }
       ));
-      const pr = (await octokit.pulls.create({ owner, repo, title: message, head: branch, base: repository.default_branch })).data;
+      const pr = (await octokit.pulls.create({ owner, repo, title: message, head: prBranch, base: repository.default_branch })).data;
       console.log(`Created PR #${pr.number}`);    
       const review = (await octokit.pulls.createReviewRequest({ owner, repo, pull_number: pr.number, team_reviewers: [ reviewTeam ]})).data;
       openUrl(`https://${gitHubUrl}/${owner}/${repo}/pull/${pr.number}`)
     }
     catch (e)
     {
-      console.log('Probably already had a .github/CODEOWNERS file (Go check)');
+      console.log(`Probably already had a ${codeOwnersPath} file (Go check)`);
     }
 }
 
